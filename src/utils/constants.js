8dba009e75e8298f357cc894cde3115b58a3eb6c
Shashank <shashanksinghal02@gmail.com>
2023-11-14 01:52:38 +0530

    - Replaced localhost with prod url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const base_url = "http://localhost:4444/api"; //Prod URL => https://quizzie-thunder-apis.onrender.com/api
+export const base_url = "https://quizzie-thunder-apis.onrender.com/api"; //Localhost URL => http://localhost:4444/api
 export const optionList = [{ id: 0, name: "A" }, { id: 1, name: "B" }, { id: 2, name: "C" }, { id: 3, name: "D" }];
